c64140d8b0260b03856dbdfc36d98e4ab15a7495
Danny Povolotski <dannypovolotski@gmail.com>
2015-02-20 20:19:10 +0200

    fixing utils

--- utils/find.js
+++ utils/find.js
@@ -1,24 +1,21 @@
-var
-	Geonames = require('../index');
-
 var argv = require('optimist')
   .usage('Usage: $0 -d [mongodb://localhost/database] -p "New york"')
   .demand(['d', 'p'])
   .alias('d', 'database')
   .describe('d', 'Mongo DB URI, e.g. mongodb://localhost/database')
   .alias('p', 'place')
-  .describe('place', 'Place, e.g. New York')  
+  .describe('place', 'Place, e.g. New York')
   .alias('u', 'utc_offset')
   .default('u', -4)
-  .describe('utc_offset', 'UTC Offset, e.g. -8')  
+  .describe('utc_offset', 'UTC Offset, e.g. -8')
   .argv;
 
-var db = Geonames.connect(argv.database);
-
+var
+	Geonames = require('../models/Geoname.js')(argv.database);
 
 	console.log('******* Place ', argv.place, argv.utc_offset, ' *********');
-	Geonames.findByLocation(argv.place, argv.utc_offset, function(err, results) {
+	Geonames.findByNameAndUTCOffset(argv.place, argv.utc_offset, function(err, results) {
+		console.log('err', err, results);
 		console.log('WINNER', results);
 		process.exit();
 	});
-	
